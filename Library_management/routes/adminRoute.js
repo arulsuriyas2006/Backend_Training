71de4ab461ef29f3a9a9b615419d5f4fd25a50c6
arulsuriyas2006 <arulsuriyas2006@gmail.com>
2025-12-17 14:21:53 +0530

    role based authentication

--- Library_management/routes/adminRoute.js
+++ Library_management/routes/adminRoute.js
@@ -1,8 +1,6 @@
 const express = require('express')
 const router = express.Router();
-const mongoose  = require('mongoose')
 const bcrypt = require('bcrypt')
-const validator = require('validator')
 const User =require('../model/userModel')
 
 
